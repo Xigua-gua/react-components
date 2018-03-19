ef988538aabb621daa5d45aa17d06335bbfa5f51
tom <646293529@qq.com>
2018-03-19 14:34:49 +0800

    qw

--- src/index.js
+++ src/index.js
@@ -12,49 +12,9 @@ import {
 import AppNavigator from './navigator'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      hidden: false,
-    }
-    this._locationUploading = false
-    // const { actions } = this.props
-    // actions.checkAuth()
-  }
-  onBackAndroid() {
-    let BACKPRESSTIMES = 0
-    if (this.nav) {
-      if (this.nav.props.navigation.state.index > 0) {
-        this.nav.props.navigation.goBack()
-        return true
-      }
-      if (this.lastBackPressed && this.lastBackPressed + 2000 >= Date.now()) {
-        return false
-      }
-      this.lastBackPressed = Date.now()
-      ToastAndroid.show("再按一次退出应用", ToastAndroid.SHORT)
-      return true
-    }
-  }
-  componentDidMount() {
-    if (Platform.OS === 'android') {
-      BackHandler.addEventListener('hardwareBackPress',() => this.onBackAndroid())
-    }
-  }
-  componentWillUnmount() {
-    if (Platform.OS === 'android') {
-      BackHandler.removeEventListener('hardwareBackPress', () => this.onBackAndroid())
-    }
-  }
   render() {
     return (
-        <AppNavigator 
-          ref={(ref) => { this.nav = ref }}
-          navigation={addNavigationHelpers({
-            dispatch: this.props.dispatch,
-            state: this.props.nav,
-          })}
-        />
+        <AppNavigator />
     )
   }
 }
